177dfddc793a8309d68fbbb6e9eddc6c0108535b
Aleksei Burlakov <alex_burlakov@bk.ru>
2018-08-03 11:18:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-  printf("Simple Build. This project was built in OBS. Release-1\n");
+  printf("Simple Build. This project was built in OBS. Release-2 (03.08.2018)\n");
   return 0;
 }
