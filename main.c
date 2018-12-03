4561bd710fd97dbc9779cf6f82e8fd94bea72069
Aleksei Burlakov <aleksei.burlakov@suse.com>
2018-12-03 18:32:07 +0100

    * fix dummy issue 1

--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main (int argc, char *argv[]) {
   printf("Simple Build. This project was built in OBS. Release-3 (06.08.2018)\n");
+  printf("A dummy change to fix a dummy issue 1");
   return 0;
 }
